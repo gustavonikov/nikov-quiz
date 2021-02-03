9eb9ace405d2145f33ac0703b8ded4b95434ea23
Gustavo <gustavonikov@gmail.com>
2021-02-03 14:59:05 -0300

    Update: Loader css

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router'
 
 const LoadingPage = styled.div`
     width: 100%;
-    height: 100vw;
+    height: 100vh;
 
     background-color: #000;
     background-position: center;
